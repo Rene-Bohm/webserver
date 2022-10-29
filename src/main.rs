9263dcd01beb976768109ab1c50b801667a022e4
Rene-Bohm <rene.kassbohm@gmail.com>
2022-10-29 16:28:28 +0200

    added health checks

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use webserver::run;
+use std::net::TcpListener;
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+
+    let listener = TcpListener::bind("127.0.0.1:0")?;
+
+    run(listener)?.await
+}
